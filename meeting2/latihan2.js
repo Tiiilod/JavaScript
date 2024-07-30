ddc2f5480f4315cf551d06c3ab3b6a17de2cb568
Ray-Auditore <151702591+Ray-Auditore@users.noreply.github.com>
2024-07-30 11:45:27 +0700

    JavaScript 2

---
+++ meeting2/latihan2.js
@@ -0,0 +1,14 @@
+// soal 2
+let nama = "turky";
+let umur = "15";
+let tempatTinggal ="jakarta";
+console.log(nama+umur+tempatTinggal); 
+// soal 3 
+let nilai = null;
+console.log(nilai)
+//soal4
+let alas = 5;
+let tinggi = 7;
+let rumus = 1/2;
+let hasil = alas * tinggi * rumus
+hasil
