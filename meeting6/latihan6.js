f3bc0a2f5ee0226541683b45ae50b9a26f02756c
tiiilod <144977755+RaffaHitipeuw@users.noreply.github.com>
2024-08-09 10:37:08 +0700

    IT latihan6

---
+++ meeting6/latihan6.js
@@ -0,0 +1,26 @@
+// no 1
+const infodiri = ["Turky", "15 Tahun", "Jakarta"];
+console.log(infodiri[1]);
+// no 2
+const namakhir = ["Turky", "15 Tahun", "Jakarta"];
+namakhir[0] = "hafizh";
+console.log(namakhir);
+// no 3
+const spilce = ["Turky", "15 Tahun", "Jakarta"];
+spilce.splice(1, 2);
+console.log(spilce);
+// no 4
+const satu = ["Turky", "15 Tahun", "Jakarta"];
+const dua = ["SBS", "16 Tahun", "Jakpus"];
+const tiga = satu.concat(dua);
+console.log(tiga);
+// no 5
+const murid = ["hamam", "hemem", "himim"];
+console.log(murid.indexOf("hemem"));
+// no 6
+const nomor = ["6", "5", "4", "2", "3", "1"];
+nomor.sort();
+console.log(nomor);
+// no 7
+const nomer = ["6", "5", "4", "2", "3", "1"];
+console.log(nomer.length);
