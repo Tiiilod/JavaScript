4b9330b157fc1a1c745e233d72c69a5f317e7a65
Tiiilod <160696449+Tiiilod@users.noreply.github.com>
2024-08-13 13:56:16 +0700

    Update latihan7.js

--- meeting7/latihan7.js
+++ meeting7/latihan7.js
@@ -1,4 +1,4 @@
-// soal 1
+// soal 1 Object Literial
 const person = {
     name: "turky",
     age: 15,
@@ -8,4 +8,38 @@ const person = {
     },
 };
 
-console.log(person['name']);
+console.log(person.addres.city);
+
+console.log(person['name'])
+console.log(person.name)
+
+
+person.age = person.age+5;
+console.log(person.age)
+
+
+delete person.addres.city;
+console.log(person.addres.city);
+
+
+// soal 2 Destructuring object
+
+//Destructuring dengan Nama Variable yang Sama
+const { name, age, } = person;
+console.log(name, age,);
+
+// Destructuring dengan Nama Variabe yang Berbeda
+const { name:userName, age:userAge} = person;
+console.log(userName, userAge)
+
+
+// soal 3 Class
+class book{
+    constructor(tittle, author) {
+        this.tittle = tittle;
+        this.author = author;
+    }
+    getDetails() {
+        return `This is ${this.tittle}, a ${this.author}.`;
+    }   
+}
