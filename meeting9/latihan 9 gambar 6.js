5eeb882cb4c6e9f00f0a35fda8a48a4cd1ebac35
Tiiilod <turky2202@student.abudzar.sch.id>
2024-09-04 10:33:05 +0700

    update meeting 9

---
+++ meeting9/latihan 9 gambar 6.js
@@ -0,0 +1,37 @@
+class Karyawan {
+   constructor(nama, gaji) {
+    var _nama = nama;
+    var _gaji = gaji;
+
+        this.getNama = function() {
+            return _nama;
+        };
+
+        this.setNama = function(nama) {
+            _nama = nama;
+        };
+
+        this.getGaji = function() {
+            return _gaji;
+        };
+
+        this.setGaji = function(gaji) {
+            _gaji = gaji;
+     };
+   }
+
+}
+
+class Manajer extends Karyawan {
+    constructor(nama, gaji) {
+        super(nama, gaji);
+    }
+    buatLaporan() {
+        return `Manager ${this.getNama()} membuat laporan.`;
+    }
+}
+
+let manager = new Manajer("Turky", 99999999);
+console.log(manager.getNama()); 
+console.log(manager.getGaji()); 
+console.log(manager.buatLaporan());
