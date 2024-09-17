6abc23ad79ed22dc2586b57a61fa97c4c639a757
Tiiilod <turky2202@student.abudzar.sch.id>
2024-09-17 14:40:33 +0700

    update

--- meeting13/latihan 13 gambar 2.js
+++ meeting13/latihan 13 gambar 2.js
@@ -1,5 +1,5 @@
 function checkNumber(index) {
-    const numbers = [3, 7, 1, 6];
+    const numbers = [3, 7, 1, 6]; // Array numbers berisi empat angka: 3, 7, 1, dan 6.
     return new Promise((resolve, reject) => {
         const number = numbers[index];
         if (number > 5) {
