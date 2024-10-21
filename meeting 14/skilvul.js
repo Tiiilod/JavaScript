7447ea971be6c3f53be3d435d5a549dd418862e4
Tiiilod <turky2202@student.abudzar.sch.id>
2024-10-21 13:33:21 +0700

    update meeting 20

--- meeting 14/skilvul.js
+++ meeting 14/skilvul.js
@@ -1,23 +1,37 @@
 // Async & await
-function helloWorld() {
-    return new Promise((resolve) => {
-      setTimeout(() => {
-        resolve("Hello World!");
-      }, 2000);
-    });
-  }
+// function helloWorld() {
+//     return new Promise((resolve) => {
+//       setTimeout(() => {
+//         resolve("Hello World!");
+//       }, 2000);
+//     });
+//   }
   
   
-  async function messages() {
-    try {
-      const msg = await helloWorld();
-      console.log(msg);
-    } catch (error) {
-      console.error(error);
-    }
-  }
+//   async function messages() {
+//     try {
+//       const msg = await helloWorld();
+//       console.log(msg);
+//     } catch (error) {
+//       console.error(error);
+//     }
+//   }
   
   
-  messages();
+//   messages();
 
   
+
+
+
+  function ambilDataUser() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (users) {
+        console.log(users);
+      });
+  }
+  
+  ambilDataUser();
